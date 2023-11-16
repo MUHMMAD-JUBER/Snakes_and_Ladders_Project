39436dd46350da215b667b8ce185ec93eb72378f
Zubair <zubairyaqoob6988@gmail.com>
2023-11-16 16:01:59 +0530

    Added : check dice where play or no play

--- src/com/BridgeLabz/Snakes_and_Ladder_game/SnakesAndLadder.java
+++ src/com/BridgeLabz/Snakes_and_Ladder_game/SnakesAndLadder.java
@@ -6,10 +6,11 @@ public class SnakesAndLadder {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
-//        To initialise the 
 
+//        To initialise the position of player
         int PLAYER1_POSITION = 0;
 
+//        To get Random number by dice for player1
         System.out.println("WELCOME TO JUNGLE SNAKE AND LADDER !!");
         while( PLAYER1_POSITION < 100){
             System.out.println("Press 'ENTER' to Roll the Dice !");
@@ -17,6 +18,34 @@ public class SnakesAndLadder {
 
             int diceRoll = rd.nextInt(6)+1;
             System.out.println("You got after roll the dice: " + diceRoll);
+
+            PLAYER1_POSITION = PLAYER1_POSITION + diceRoll;
+
+//            For checking case by the player
+            int number = rd.nextInt(3);
+
+            switch(number){
+                case 0:
+                    System.out.println(" In no Play condition: " + PLAYER1_POSITION + " ");
+                            break;
+//                To jump between 1 to 20
+                case 1:
+                    int ladderJump = rd.nextInt(20) + 1;
+                    System.out.println(" Player1 chose to climb over there !"+ (PLAYER1_POSITION + ladderJump) +" ");
+                    PLAYER1_POSITION = PLAYER1_POSITION + ladderJump;
+                            break;
+//                To slide down between 1 to 20
+                case 2:
+                    int snakeSlide = rd.nextInt(20) + 1;
+                    System.out.println("Player1 chose to slide down over there !"+ (PLAYER1_POSITION + snakeSlide) + " ");
+                    PLAYER1_POSITION = PLAYER1_POSITION - snakeSlide;
+                            break;
+                default:
+                    System.out.println("Invalid option No Play: " + PLAYER1_POSITION);
+                            break;
+
+            }
+
 
 
         }
