c37c368a46a0c15021f395517353be430df33f1e
Zubair <zubairyaqoob6988@gmail.com>
2023-11-16 13:40:50 +0530

    Added : to roll the dice for getting random number

--- src/com/BridgeLabz/Snakes_and_Ladder_game/SnakesAndLadder.java
+++ src/com/BridgeLabz/Snakes_and_Ladder_game/SnakesAndLadder.java
@@ -1,11 +1,25 @@
 package com.BridgeLabz.Snakes_and_Ladder_game;
+
 import java.util.*;
 import java.util.Random;
 public class SnakesAndLadder {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+//        To initialise the 
+
         int PLAYER1_POSITION = 0;
 
         System.out.println("WELCOME TO JUNGLE SNAKE AND LADDER !!");
+        while( PLAYER1_POSITION < 100){
+            System.out.println("Press 'ENTER' to Roll the Dice !");
+            sc.nextLine();
+
+            int diceRoll = rd.nextInt(6)+1;
+            System.out.println("You got after roll the dice: " + diceRoll);
+
+
+        }
 
     }
 }
